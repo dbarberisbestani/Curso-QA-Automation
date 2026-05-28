b8db58a582abedeed21ab4bbb22d18daba5d9860
diegoexaz@gmail.com <diegoexaz@gmail.com>
2026-05-28 18:33:35 -0300

    clase 6

---
+++ cypress/e2e/sincomandos.cy.js
@@ -0,0 +1,104 @@
+describe('SauceDemo - Sin comandos personalizados', () => {
+
+  it('Compra completa con un producto', () => {
+
+    // Login
+    cy.visit('https://www.saucedemo.com/')
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+    cy.url().should('include', '/inventory.html')
+
+    // Agregar producto al carrito
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+    cy.get('.shopping_cart_badge').should('have.text', '1')
+
+    // Ir al carrito
+    cy.get('.shopping_cart_link').click()
+    cy.url().should('include', '/cart.html')
+    cy.get('.cart_item').should('have.length', 1)
+
+    // Iniciar checkout
+    cy.get('[data-test="checkout"]').click()
+    cy.url().should('include', '/checkout-step-one.html')
+
+    // Completar formulario
+    cy.get('[data-test="firstName"]').type('Juan')
+    cy.get('[data-test="lastName"]').type('Pérez')
+    cy.get('[data-test="postalCode"]').type('5000')
+    cy.get('[data-test="continue"]').click()
+    cy.url().should('include', '/checkout-step-two.html')
+
+    // Confirmar pedido
+    cy.get('[data-test="finish"]').click()
+    cy.get('.complete-header').should('have.text', 'Thank you for your order!')
+
+  })
+
+  
+  it('Compra completa con dos productos', () => {
+
+    // Login 
+    cy.visit('https://www.saucedemo.com/')
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+    cy.url().should('include', '/inventory.html')
+
+    // Agregar dos productos
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+    cy.get('.shopping_cart_badge').should('have.text', '1')
+    cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click()
+    cy.get('.shopping_cart_badge').should('have.text', '2')
+
+    // Ir al carrito
+    cy.get('.shopping_cart_link').click()
+    cy.url().should('include', '/cart.html')
+    cy.get('.cart_item').should('have.length', 2)
+
+    // Iniciar checkout
+    cy.get('[data-test="checkout"]').click()
+    cy.url().should('include', '/checkout-step-one.html')
+
+    // Completar formulario — igual que antes
+    cy.get('[data-test="firstName"]').type('Juan')
+    cy.get('[data-test="lastName"]').type('Pérez')
+    cy.get('[data-test="postalCode"]').type('5000')
+    cy.get('[data-test="continue"]').click()
+    cy.url().should('include', '/checkout-step-two.html')
+
+    // Confirmar pedido
+    cy.get('[data-test="finish"]').click()
+    cy.get('.complete-header').should('have.text', 'Thank you for your order!')
+
+  })
+
+ 
+  it('Logout exitoso después de una compra', () => {
+
+    // Login 
+    cy.visit('https://www.saucedemo.com/')
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+    cy.url().should('include', '/inventory.html')
+
+    // Agregar producto y completar compra
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+    cy.get('.shopping_cart_link').click()
+    cy.get('[data-test="checkout"]').click()
+    cy.get('[data-test="firstName"]').type('Juan')
+    cy.get('[data-test="lastName"]').type('Pérez')
+    cy.get('[data-test="postalCode"]').type('5000')
+    cy.get('[data-test="continue"]').click()
+    cy.get('[data-test="finish"]').click()
+    cy.get('.complete-header').should('have.text', 'Thank you for your order!')
+
+    // Logout
+    cy.get('#react-burger-menu-btn').click()
+    cy.get('#logout_sidebar_link').should('be.visible').click()
+    cy.url().should('eq', 'https://www.saucedemo.com/')
+
+  })
+
+})
